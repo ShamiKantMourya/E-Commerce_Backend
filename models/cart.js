f774128641399cc77c9b0cef70dc28dbd017beac
Shami_Mourya <shamimourya9852@gmail.com>
2023-11-25 21:24:49 +0530

    code formatting

--- models/cart.js
+++ models/cart.js
@@ -48,6 +48,9 @@ module.exports = class Cart {
       }
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((product) => product.id === id);
+      if (!product) {
+        return;
+      }
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
@@ -70,5 +73,5 @@ module.exports = class Cart {
         cb(cart);
       }
     });
-  }
+  };
 };
