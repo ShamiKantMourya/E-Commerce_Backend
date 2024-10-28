46419ca80726d0177eeaaf37524b1af276ae1559
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-28 09:51:53 +0530

    code refactor

--- routes/shop.js
+++ routes/shop.js
@@ -12,14 +12,14 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 
-router.post('/cart', shopController.postCart);
+// router.post('/cart', shopController.postCart);
 
-router.post('/delete-cart-product', shopController.deleteCartProduct);
+// router.post('/delete-cart-product', shopController.deleteCartProduct);
 
-router.get('/orders', shopController.getOrders);
+// router.get('/orders', shopController.getOrders);
 
-router.get('/checkout', shopController.getCheckout);
+// router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
