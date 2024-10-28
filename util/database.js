655f0b60c627720a270cfeecdda7199a08925d1d
ShamiKantMourya <shamimourya9852@gmail.com>
2024-10-28 09:50:56 +0530

    mongoDb created

--- util/database.js
+++ util/database.js
@@ -1,10 +1,30 @@
-const mySql = require('mysql2');
+const mongoDb = require("mongodb");
 
-const connectionPool = mySql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'e-commerce',
-    password: '@Arnav9852!'
-});
+const MongoClient = mongoDb.MongoClient;
 
-module.exports = connectionPool.promise();
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://ShamiMourya:Arnav9852@cluster.ytoyh.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster"
+  )
+    .then((client) => {
+      console.log("Database connected successfully");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+if(_db){
+  return _db
+}
+throw "No database found..!"
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
