6a88ff23b4831eef88aaa87d20acbb27686e5101
ShamiKantMourya <shamimourya9852@gmail.com>
2024-11-04 21:04:47 +0530

    mongoose database

--- util/database.js
+++ util/database.js
@@ -1,31 +1,46 @@
-const mongoDb = require("mongodb");
+// const mongoDb = require("mongodb");
 
-const MongoClient = mongoDb.MongoClient;
+// const MongoClient = mongoDb.MongoClient;
 
-let _db;
+// let _db;
+// const mongoDB_URI = process.env.MongoDB_URI;
+
+// const mongoConnect = (callback) => {
+//   MongoClient.connect(mongoDB_URI)
+//     .then((client) => {
+//       console.log("Database connected successfully");
+//       _db = client.db();
+//       callback();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       throw err;
+//     });
+
+// };
+
+// const getDb = () => {
+//   // console.log("getDb called");
+//   if (_db) {
+//     return _db;
+//   }
+//   throw "No database found..!";
+// };
+
+// exports.mongoConnect = mongoConnect;
+// exports.getDb = getDb;
+
+const mongoose = require("mongoose");
+
 const mongoDB_URI = process.env.MongoDB_URI;
 
-const mongoConnect = (callback) => {
-  MongoClient.connect(mongoDB_URI)
-    .then((client) => {
-      console.log("Database connected successfully");
-      _db = client.db();
-      callback();
-    })
-    .catch((err) => {
-      console.log(err);
-      throw err;
-    });
-  
-};
+const dataBaseConnect = () => {
+  mongoose
+  .connect(mongoDB_URI)
+  .then(console.log("Database connected"))
+  .catch((err) => {
+    console.log(err);
+  });
+}
 
-const getDb = () => {
-  // console.log("getDb called");
-  if (_db) {
-    return _db;
-  }
-  throw "No database found..!";
-};
-
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
+  module.exports = dataBaseConnect;
