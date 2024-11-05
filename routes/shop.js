dc9c82054b4b865966874f58f8ded0002e2ce9e8
ShamiKantMourya <shamimourya9852@gmail.com>
2024-11-05 20:19:45 +0530

    commented other routes

--- routes/shop.js
+++ routes/shop.js
@@ -14,20 +14,20 @@ router.get('/products', shopController.getProducts);
 //Get product details
 router.get('/products/:productId', shopController.getProduct);
 
-//Get cart product
-router.get('/cart', shopController.getCartProducts);
+// //Get cart product
+// router.get('/cart', shopController.getCartProducts);
 
-//Add to cart
-router.post('/cart', shopController.postCart);
+// //Add to cart
+// router.post('/cart', shopController.postCart);
 
-//Delete cart product
-router.post('/delete-cart-product', shopController.deleteCartProduct);
+// //Delete cart product
+// router.post('/delete-cart-product', shopController.deleteCartProduct);
 
-//Create order
-router.post('/create-order', shopController.postOrders);
+// //Create order
+// router.post('/create-order', shopController.postOrders);
 
-//Get Order
-router.get('/orders', shopController.getOrder);
+// //Get Order
+// router.get('/orders', shopController.getOrder);
 
 // router.get('/checkout', shopController.getCheckout);
 
