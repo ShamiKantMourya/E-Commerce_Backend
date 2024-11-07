d6b3f954b95a1bfa6a1c3b625f5b60583b5f3152
ShamiKantMourya <shamimourya9852@gmail.com>
2024-11-07 20:46:22 +0530

    order schema

---
+++ models/order.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  products: [
+    {
+      product: {
+        type: Object,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
