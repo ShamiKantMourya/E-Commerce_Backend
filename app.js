09e1b802daa1d175f370ca25c494ee235c54b3f1
ShamiKantMourya <shamimourya9852@gmail.com>
2024-11-12 20:03:39 +0530

    auth route

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -51,6 +52,7 @@ User.findOne().then((user) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
