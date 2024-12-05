c981dd0318490a3b162c54be71b73f8f4583a530
ShamiKantMourya <shamimourya9852@gmail.com>
2024-12-05 21:23:05 +0530

    defined routes

--- routes/auth.js
+++ routes/auth.js
@@ -4,14 +4,19 @@ const authController = require("../controllers/auth");
 
 const router = express.Router();
 
+//Login route
 router.get("/login", authController.getLogin);
 
+//Signup Route
 router.get('/signup', authController.getSignup);
 
+//User login route
 router.post("/login", authController.postLogin);
 
+//User logout route
 router.post('/logout', authController.postLogout);
 
+//User signup route
 router.post('/signup', authController.postSignup);
 
 module.exports = router;
